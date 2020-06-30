6c12d823deeac6facc98ba140b6b6cb98afe8cda
Sigita Senkiene <senkiene.sigita@gmail.com>
2020-06-30 15:06:32 +0300

    Add chokidar, watch for add, change,unlinks events

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
-console.log('executed');
+const chokidar = require('chokidar');
+
+chokidar.watch('.')
+    .on('add', () => console.log('file added'))
+    .on('change', () => console.log('file changed'))
+    .on('unlink', () => console.log('file unlinked'));
